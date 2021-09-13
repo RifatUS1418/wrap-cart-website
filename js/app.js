8e1ed26afa0b03f7292f171f9bad9940f4de81b5
unknown <rifatullah1418@gmail.com>
2021-09-13 10:48:55 +0600

    search button styled

--- js/app.js
+++ js/app.js
@@ -20,6 +20,8 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <small>Rating count: ${product.rating.count}</small>;
+      <small>Rating rate: ${product.rating.rate}</small>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
@@ -79,5 +81,5 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
